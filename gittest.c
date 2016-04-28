59ee7095efac9e6f33495bfe03b15d7cd0f65b89
许宏杰 <JackyXu@xuhongjiedeMacBook-Air.local>
2016-04-28 16:44:40 +0800

    modify two class

--- gittest.c
+++ gittest.c
@@ -1,1 +1,9 @@
+//
+//  gittest.c
+//  test
+//
+//  Created by 许宏杰 on 16/4/28.
+//  Copyright © 2016年 许宏杰. All rights reserved.
+//
+
 #include "gittest.h"
--- gittest.h
+++ gittest.h
@@ -1,3 +1,11 @@
+//
+//  gittest.h
+//  test
+//
+//  Created by 许宏杰 on 16/4/28.
+//  Copyright © 2016年 许宏杰. All rights reserved.
+//
+
 #ifndef gittest_h
 #define gittest_h
 
